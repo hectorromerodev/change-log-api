f4cffbf16bc87b4559c9ca3779d5fbf2b20bcaef
Hector Romero <hectorromerodev@gmail.com>
2022-12-20 21:50:20 -0800

    add middleware server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,23 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
 import router from './router';
 
 const app = express();
+
+// Settings
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Custom middleware
+app.use((req, res, next) => {
+    console.log('Custom middleware - Time: ', Date.now());
+    next();
+});
+
 
 app.get('/', (req, res) => {
     res.status(200);
