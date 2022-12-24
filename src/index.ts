c34c8877234f4884005f2dab0caa776e0630e3e3
Hector Romero <hectorromerodev@gmail.com>
2022-12-23 22:38:52 -0800

    update index server with new config env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import config from './config';
 import app from './server';
 
+const PORT = config.port || 3999;
 
-app.listen(3000, () => {
-    console.log('Server on http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Server on http://localhost:${PORT}`);
 });
