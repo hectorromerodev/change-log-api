14c4ba4a99333eb809b33371956488f8bcbb4839
Hector Romero <hectorromerodev@gmail.com>
2022-12-23 22:34:40 -0800

    update try catch for errors

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,39 +1,42 @@
+import e from 'express';
 import prisma from '../db';
 import { hashPassword, createJWT, comparePassword } from '../modules/auth';
-import type { Request, Response } from 'express';
+import type { NextFunction, Request, Response } from 'express';
 
-export const createUser = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    const user = await prisma.user.create({
-        data: {
-            username,
-            password: await hashPassword(password),
-        },
-    });
-
-    const token = createJWT(user);
-
-    return res.status(201).json({ token });
+export const createUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { username, password } = req.body;
+        const user = await prisma.user.create({
+            data: {
+                username,
+                password: await hashPassword(password),
+            },
+        });
+        const token = createJWT(user);
+        res.status(201).json({ token });
+    }catch(err: any) {
+        err.type = 'input';
+        next(err);
+    }
 }
 
-export const signIn = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    
-    const user = await prisma.user.findUnique({
-        where: {
-            username,
-        },
-    });
-    if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+export const signIn = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { username, password } = req.body;
+        const user = await prisma.user.findUnique({
+            where: {
+                username,
+            },
+        });
+        if (!user) return res.status(404).json({ message: 'User not found' });
+        
+        const isPasswordValid = await comparePassword(password, user.password);
+        if (!isPasswordValid) return res.status(401).json({ message: 'Invalid password' });
+        
+        const token = createJWT(user);
+        res.status(200).json({ token });
+    } catch(err: any){
+        err.type = 'input';
+        next(err);
     }
-
-    const isPasswordValid = await comparePassword(password, user.password);
-    if (!isPasswordValid) {
-        return res.status(401).json({ message: 'Invalid password' });
-    }
-
-    const token = createJWT(user);
-
-    return res.status(200).json({ token });
 }
